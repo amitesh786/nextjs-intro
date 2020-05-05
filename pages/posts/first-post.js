47232aa1b3882ca04134bc7ca50b3ebce44f6b9b
Amitesh Singh <amitesh.singh@corvallis.it>
2020-05-05 14:20:04 +0200

    [change] look update

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,20 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+
             <h1>First Post</h1>
             <h2>
                 <Link href="/posts">
-                    <a>Back to post</a>
+                <a>Back to post</a>
                 </Link>
             </h2>
-        </>
+        </Layout>
     )
 }
