98989f6bcfd4cabca925a1e5f5ea94de5e6a364f
Amitesh Singh <amitesh.singh@corvallis.it>
2020-05-06 12:48:15 +0200

    [change] layout + package + style modify

--- components/layout.js
+++ components/layout.js
@@ -59,7 +59,7 @@ export default function Layout({ children, home }) {
             <main>{children}</main>
             { !home && (
                 <div className={styles.backToHome}>
-                <Link href="/posts">
+                <Link href="/">
                     <a>← Back to home</a>
                 </Link>
                 </div>
